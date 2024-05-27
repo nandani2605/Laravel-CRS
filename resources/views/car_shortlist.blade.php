9de611658703269100975c48ef9ac53a70463757
Nandini Patel <nandini.wingersitservices@gmail.com>
2024-05-27 17:26:03 +0530

    add the blade files

---
+++ resources/views/car_shortlist.blade.php
@@ -0,0 +1,214 @@
+@extends('master')
+@section('title', 'Car Shortlist')
+@section('content')
+<div class="split left">
+    <div class="centered">
+        <img src="img_avatar2.png" alt="Avatar woman">
+        <h2>Jane Flex</h2>
+        <p>
+        <h1>
+            <center> The perfect car for your next trip is just around the corner</center>
+        </h1>
+        </p>
+        <p>
+        <h2>
+            <center>Book your drive now!</center>
+        </h2>
+        </p>
+    </div>
+</div>
+
+<!--Car Short List-->
+<div class="float-container">
+
+    <div class="float-child-1">
+        <br>
+        <h3>Filter</h3>
+        <form>
+            <br>
+            <b>Segment</b><br>
+            <input type="checkbox" id="Hatchback" name="f1-segment" value="Hatchback">Hatchback<br>
+            <input type="checkbox" id="Sedan" name="f1-segment" value="Sedan">Sedan<br>
+            <input type="checkbox" id="SUV" name="f1-segment" value="SUV">SUV<br>
+            <input type="checkbox" id="MUV" name="f1-segment" value="MUV">MUV
+            <br>
+            <br>
+            <b>Brand</b><br>
+            <input type="checkbox" name="f1-brand" value="Maruti">Maruti<br>
+            <input type="checkbox" name="f1-brand" value="Hyundai">Hyundai<br>
+            <input type="checkbox" name="f1-brand" value="Mahindra">Mahindra<br>
+            <input type="checkbox" name="f1-brand" value="Honda">Honda
+            <br>
+            <br>
+            <b>Fule Type</b><br>
+            <input type="checkbox" name="f1-type" value="Diesel">Diesel<br>
+            <input type="checkbox" name="f1-type" value="Petrol">Petrol<br>
+            <br>
+            <b>Seating Capacity</b><br>
+            <input type="checkbox" name="f1-capacity" value="5_seats">5 seats<br>
+            <input type="checkbox" name="f1-capacity" value="7_seats">7 seats<br>
+            <br>
+            <br>
+            <input type="reset" id="reset" value="ClearAll">
+        </form>
+    </div>
+
+
+    <div class="float-child-2">
+        <!--<section class="services" id="services"></section>
+            <div class="heading">
+                <span>Best Services</span>
+                <h1>Explore Out Top Deals <br> From Top Rated Dealers</h1>
+            </div>-->
+        <div class="services-container">
+            <div class="box">
+                <div class="box-img">
+                    <img src="assets/images/car1.jpg" alt="car1">
+                </div>
+                <p></p>
+                <h3>Nissan Kicks 2021</h3>
+                <h2>₹251 <span>/hr</span></h2>
+                <a href="nissan_kicks.html" class="btn">Rent Now</a>
+            </div>
+
+            <div class="box">
+                <div class="box-img">
+                    <img src="assets/images/car2.jpg" alt="car1">
+                </div>
+                <p></p>
+                <h3>Mahindra XUV500 </h3>
+                <h2> ₹300 <span>/hr</span></h2>
+                <a href="xuv500.html" class="btn">Rent Now</a>
+            </div>
+
+            <div class="box">
+                <div class="box-img">
+                    <img src="assets/images/car3.jpg" alt="car1">
+                </div>
+                <p></p>
+                <h3>KIA Sonet 2021</h3>
+                <h2>₹420 <span>/hr</span></h2>
+                <a href="KiaSonet.html" class="btn">Rent Now</a>
+            </div>
+            <div class="box">
+                <div class="box-img">
+                    <img src="assets/images/car4.jpg" alt="car1">
+                </div>
+                <p></p>
+                <h3>Maruti Wagon R </h3>
+                <h2>₹200 <span>/hr</span></h2>
+                <a href="Maruti_WagonR.html" class="btn">Rent Now</a>
+            </div>
+
+            <div class="box">
+                <div class="box-img">
+                    <img src="assets/images/car5.jpg" alt="car1">
+                </div>
+                <p></p>
+                <h3>2018 Honda Civic</h3>
+                <h2> ₹300 <span>/hr</span></h2>
+                <a href="hondaCivic.html" class="btn">Rent Now</a>
+            </div>
+
+            <div class="box">
+                <div class="box-img">
+                    <img src="assets/images/car6.jpg" alt="car1">
+                </div>
+                <p></p>
+                <h3>Hyundai Grand i10 </h3>
+                <h2> ₹190 <span>/hr</span></h2>
+                <a href="i10.html" class="btn">Rent Now</a>
+            </div>
+            <div class="box">
+                <div class="box-img">
+                    <img src="assets/images/car7.jpg" alt="car1">
+                </div>
+                <p></p>
+                <h3>Honda Amaze 2018</h3>
+                <h2>₹350 <span>/hr</span></h2>
+                <a href="hondaAmaze.html" class="btn">Rent Now</a>
+            </div>
+            <div class="box">
+                <div class="box-img">
+                    <img src="assets/images/car8.jpg" alt="car1">
+                </div>
+                <p></p>
+                <h3>Hyundai Creta 2016</h3>
+                <h2>₹195 <span>/hr</span></h2>
+                <a href="creta.html" class="btn">Rent Now</a>
+            </div>
+            <div class="box">
+                <div class="box-img">
+                    <img src="assets/images/car9.jpg" alt="car1">
+                </div>
+                <p></p>
+                <h3>Maruti Brezza 2020</h3>
+                <h2>₹260 <span>/hr</span></h2>
+                <a href="brezza.html" class="btn">Rent Now</a>
+            </div>
+            <div class="box">
+                <div class="box-img">
+                    <img src="assets/images/car10.jpg" alt="car1">
+                </div>
+                <p></p>
+                <h3>Mahindra Marazzo 2018</h3>
+                <h2>₹300 <span>/hr</span></h2>
+                <a href="marazzo.html" class="btn">Rent Now</a>
+            </div>
+            <div class="box">
+                <div class="box-img">
+                    <img src="assets/images/car11.jpg" alt="car1">
+                </div>
+                <p></p>
+                <h3>Renault Triber 2022</h3>
+                <h2>₹368 <span>/hr</span></h2>
+                <a href="tiber.html" class="btn">Rent Now</a>
+            </div>
+            <div class="box">
+                <div class="box-img">
+                    <img src="assets/images/car12.jpg" alt="car1">
+                </div>
+                <p></p>
+                <h3>Ford Ecosport 2016</h3>
+                <h2>₹162 <span>/hr</span></h2>
+                <a href="ecosport.html" class="btn">Rent Now</a>
+            </div>
+        </div>
+        <br>
+        <br>
+
+        <p>
+        <h2>
+            <center>Largest car sharing marketplace
+                Sedans for short distances, SUVs for tough terrains, luxury cars for surprises - we’ve got it all!</center>
+        </h2>
+        </p>
+        <br> <br>
+        <p>
+        <h2>
+            <center>UNMATCHED BENEFITS</center>
+        </h2>
+        </p>
+        <p>
+        <h2>
+            <center>Drive everywhere with freedom</center>
+        </h2>
+        </p>
+        <br>
+        <p><b>
+                <center>Enjoy Unlimited Kilometers</center>
+            </b>
+            <b>
+                <center>Zero Security Deposite</center>
+            </b>
+            <b>
+                <center>100% Free Cancellation before 24 hrs of trip start</center>
+            </b>
+            <b>
+                <center>Reschedule for Free</center>
+            </b>
+        </p>
+    </div>
+</div>
+</div>
+@endsection
---
+++ resources/views/home.blade.php
@@ -0,0 +1,198 @@
+@extends('master')
+@section('title', 'Home')
+@section('content')
+<!-- Home -->
+ <section class="home" id="home">
+        <div class="text">
+            <h1><span>Looking </span>to <br> rent a car</h1>
+            <p>The perfect car for your next trip is just around the corner
+               <br> Book your drive now! </p>
+                <div class="app-stores">
+                    <img src="assets/images/ios.png" alt="reload">
+                    <img src="assets/images/512x512.png" alt="">
+            
+                </div>
+        </div>
+        <div class="form-container">
+            <form action="">
+                <div class="input-box">
+                    <span>Location</span>
+                    <input type="search" name="" id="" placeholder="search places">
+                </div>
+                    <div class="input-box">
+                        <span>Pick-Up Date</span>
+                        <input type="date" name="" id="">
+                    </div>
+                    <div class="input-box">
+                        <span>Return Date</span>
+                        <input type="date" name="" id="">
+                    </div>
+                    <input type="submit" name="" id="" class="btn">
+                </div>
+            </form>
+        </div>
+    </section>
+    <!--ride section-->
+    <section class="ride" id="ride">
+        <div class="heading">
+            <span>How Its Work</span>
+            <h1>Rent With 3 Easy Steps</h1>
+        </div>
+        <div class="ride-container">
+            <div class="box">
+                <i class='bx bxs-map'></i>
+                <h2>Choose A Location</h2>
+                <p>Choose alocation from where you want to pick the car</p>
+            </div>
+
+            <div class="box">
+                <i class='bx bxs-calendar-check'></i>
+                <h2>Pick-Up Date</h2>
+                <p>Select the date according to your need and no of hours you want to rent</p>
+            </div>
+
+            <div class="box">
+                <i class='bx bxs-calendar-star'></i>
+                <h2>Book A Car</h2>
+                <p>Book a car by submitting the important documents and after verification the car is yours !</p>
+            </div>
+        </div>
+    </section>
+    <!-- Services -->
+    <section class="services" id="services"></section>
+    <div class="heading">
+        <span>Best Services</span>
+        <h1>Explore Out Top Deals <br> From Top Rated Dealers</h1>
+    </div>
+    <div class="services-container">
+        <div class="box">
+        <div class="box-img">
+        <img src="assets/images/car1.jpg" alt="car1">
+    </div>
+    <p></p>
+    <h3>Nissan Kicks 2021</h3>
+    <h2>₹251 <span>/hr</span></h2>
+    <a href="#" class="btn">Rent Now</a>
+</div>
+
+<div class="box">
+    <div class="box-img">
+    <img src="assets/images/car2.jpg" alt="car1">
+    </div>
+<p></p>
+<h3>Mahindra XUV500 </h3>
+<h2> ₹300 <span>/hr</span></h2>
+<a href="#" class="btn">Rent Now</a>
+</div>
+
+<div class="box">
+    <div class="box-img">
+    <img src="assets/images/car3.jpg" alt="car1">
+</div>
+<p></p>
+<h3>KIA Sonet 2021</h3>
+<h2>₹420 <span>/hr</span></h2>
+<a href="#" class="btn">Rent Now</a>
+</div>
+<div class="box">
+    <div class="box-img">
+    <img src="assets/images/car4.jpg" alt="car1">
+</div>
+<p></p>
+<h3>Maruti Wagon R </h3>
+<h2>₹200 <span>/hr</span></h2>
+<a href="#" class="btn">Rent Now</a>
+</div>
+
+<div class="box">
+    <div class="box-img">
+    <img src="assets/images/car5.jpg" alt="car1">
+</div>
+<p></p>
+<h3>2018 Honda Civic</h3>
+<h2>₹58500 | ₹358 <span>/month</span></h2>
+<a href="#" class="btn">Rent Now</a>
+</div>
+
+<div class="box">
+    <div class="box-img">
+    <img src="assets/images/car6.jpg" alt="car1">
+</div>
+<p></p>
+<h3>Hyundai Grand i10 </h3>
+<h2> ₹190 <span>/hr</span></h2>
+<a href="#" class="btn">Rent Now</a>
+</div>
+</div>
+    <!--about section-->
+    <section class="about" id="about">
+        <div class="heading">
+            <span>About Us</span>
+            <h1>Best Customer Experience</h1>
+        </div>
+        <div class="about-container">
+            <div class="about-img">
+                <img src="assets/images/about.png" alt="">
+            </div>
+            <div class="about-text">
+                <span>About Us</span>
+                <p>Our Car Rental website is India’s largest marketplace for cars on rent. From short road trips to quick in-city drives for groceries, supply pick-up, food runs, we have the cheapest car rental options for all your needs! A hatchback for daily commute, a sedan for short trips, SUV for hills or a luxury car for a surprise.</p>
+                <p>With us, you can experience the convenience of online booking. The cars listed on our platform come with all-India permits that include vehicle insurance. It is extremely easy to pick up the car from the host location. You can drive unlimited KMs, with 100% Free Cancellation up to 6 hours before the trip start, 0 Security Deposit, 0 Toll Charges, and 24/7 Roadside Assistance. Car rent per KM starts as low as Rs. 149/hour. From short road trips to quick in-city drives for groceries, supply pick-up, meeting friends and family, doctor visits, business trips, we have the cheapest car rental options for all your needs! A hatchback for daily commute, a sedan for short trips, SUV for hills or a luxury car for a surprise.</p>
+                <a href="#" class="btn">learn More</a>
+            </div>
+        </div>
+    </section>
+    <!--reviews-->
+    <section class="reviews" id="reviews">
+        <div class="heading">
+            <span>Reviews</span>
+            <h1>Whats Our Customer Says.......</h1>
+        </div>
+        <div class="review-container">
+            <div class="box">
+                <div class="rev-img">
+                    <img src="assets/images/rev1.jpg" alt="">
+            </div>
+            <h2>Arvind</h2>
+            <div class="stars">
+                <i class="bx bxs-star"></i>
+                <i class="bx bxs-star"></i>
+                <i class="bx bxs-star"></i>
+                <i class="bx bxs-star"></i>
+                <i class="bx bxs-star-half"></i>
+            </div>
+            <p>Flattered with availability of well maintained cars</p>
+        </div>
+        <div class="box">
+            <div class="rev-img">
+                <img src="assets/images/rev2.jpg" alt="">
+        </div>
+        <h2>Gaurav</h2>
+        <div class="stars">
+            <i class="bx bxs-star"></i>
+            <i class="bx bxs-star"></i>
+            <i class="bx bxs-star"></i>
+            <i class="bx bxs-star"></i>
+            <i class="bx bxs-star-half"></i>
+        </div>
+        <p>Booked a XUV with unlimited kms, very happy with car's service           <p>
+    </div>
+    <div class="box">
+        <div class="rev-img">
+            <img src="assets/images/rev3.jpg" alt="">
+    </div>
+    <h2>Himanshi </h2>
+    <div class="stars">
+        <i class="bx bxs-star"></i>
+        <i class="bx bxs-star"></i>
+        <i class="bx bxs-star"></i>
+        <i class="bx bxs-star"></i>
+        <i class="bx bxs-star-half"></i>
+    </div>
+    <p>Booked a car for a family trip which was very comfortable and in great condition
+
+    </p>
+</div>
+        </div>
+    </section>
+@endsection
